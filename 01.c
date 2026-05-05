81994358edcf81fad78d204d3b647ce76fec3cd0
JINWOO PARK <138313999+qkatoffo@users.noreply.github.com>
2026-05-06 00:50:26 +0900

    주석
    Added comments for clarity and maintained functionality.

--- 01.c
+++ 01.c
@@ -2,18 +2,18 @@
 
 #define MAX 5
 
-void makeWayPoint()
+void makeWayPoint() // 드론 지점 생성 함수
 {
-	srand((unsigned int)time(NULL));
-	FILE* fp1 = fopen("01.txt", "w");
-	fprintf(fp1, "%8d %8d\n", 0, 0);
+	srand((unsigned int)time(NULL)); // 무작위 난수값을 생성하기 위한 시드값 설정
+	FILE* fp1 = fopen("01.txt", "w"); // 01.txt 쓰기 모드로 열기
+	fprintf(fp1, "%8d %8d\n", 0, 0); // A 좌표를 (0, 0)으로 설정후 파일에 쓰기
 	printf("%8d %8d\n", 0, 0);
 
 	int i, x, y;
 	for (i = 0; i < MAX - 1; i++)
 	{
-		x = rand() % 50;
-		y = rand() % 50;
+		x = rand() % 50; // 0부터 49까지의 난수 발생
+		y = rand() % 50; // 0부터 49까지의 난수 발생
 
 		fprintf(fp1, "%8d %8d\n", x, y);
 		printf("%8d %8d\n", x, y);
