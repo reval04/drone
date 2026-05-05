6a1664c872399e78ced471a5f71060dd0bb0de0b
leehoowon3 <hoo8732@gmail.com>
2026-05-06 00:03:08 +0900

    01.c

--- 01.c
+++ 01.c
@@ -1,17 +1,13 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "drone.h"
 
 #define MAX 5
 
-int main()
+void makeWayPoint()
 {
 	srand((unsigned int)time(NULL));
 	FILE* fp1 = fopen("01.txt", "w");
 	fprintf(fp1, "%8d %8d\n", 0, 0);
 	printf("%8d %8d\n", 0, 0);
-	// int axis[2][MAX - 1];
 
 	int i, x, y;
 	for (i = 0; i < MAX - 1; i++)
@@ -21,40 +17,9 @@ int main()
 
 		fprintf(fp1, "%8d %8d\n", x, y);
 		printf("%8d %8d\n", x, y);
-	//	axis[0][i] = x;
-	//	axis[1][i] = y;
 	}
 
-	/*
-	int tempAxis[2][1];
-	for (i = 0; i < MAX - 2; i++)
-	{
-		for (j = 0; j < MAX - 2 - i; j++)
-		{
-			int distance1 = axis[0][j] * axis[0][j] + axis[1][j] * axis[1][j];
-			int distance2 = axis[0][j+1] * axis[0][j+1] + axis[1][j+1] * axis[1][j+1];
-			if (distance1 > distance2)
-			{
-				tempAxis[0][0] = axis[0][j];
-				axis[0][j] = axis[0][j + 1];
-				axis[0][j + 1] = tempAxis[0][0];
-
-				tempAxis[1][0] = axis[1][j];
-				axis[1][j] = axis[1][j + 1];
-				axis[1][j + 1] = tempAxis[1][0];
-			}
-		}
-	}
-	*/
-	/*
-	for (i = 0; i < MAX - 1; i++)
-	{
-		x = axis[0][i];
-		y = axis[1][i];
-		fprintf(fp1, "%8d %8d\n", x, y);
-	}
-	*/
-	
 	fclose(fp1);
-	system("01.txt notepad.exe");
+	printf("===================================================\n");
+	system("notepad.exe 01.txt");
 }
