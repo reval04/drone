711bcf43363f72e22f72a31c1370d7c79bc99e52
reval04 <129571354+reval04@users.noreply.github.com>
2026-05-05 12:09:27 +0900

    Implement addCoordinate function to save random coords
    Added a function to generate random coordinates and save them to a file.

--- 고예승/drone.c
+++ 고예승/drone.c
@@ -3,3 +3,15 @@ double getDistance(int x1, int y1, int x2, int y2) { // 두 지점 사이 거리
     int dy = y1 - y2;
     return sqrt((double)(dx * dx + dy * dy));
 }
+
+void addCoordnate(const char* filename) { 
+    FILE* f = fopen(filename, "a+");
+    if (f == NULL) return;
+    for (int i = 0; i < MAX; i++) {
+        int x = rand() % 500;
+        int y = rand() % 500;
+        fprintf(stdout, "(%d,%d)    ", x, y);
+        fprintf(f, "(%d,%d)    ", x, y);
+    }
+    fclose(f);
+}
