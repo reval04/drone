2756c13998867a5fe4b42e4dd77a03eb32c93dcc
reval04 <yeseunggo@gmail.com>
2026-05-24 15:53:05 +0900

    .

--- 고예승/drone/main.c
+++ 고예승/drone/main.c
@@ -75,30 +75,3 @@ int main() {
     return 0;
 }
 
-void run_step_01() {
-    FILE* fp1 = fopen("01.txt", "a+");
-    if (fp1 == NULL) return;
-
-    printf("\n웨이포인트 생성 결과:\n");
-
-    // 파일과 화면 상단에 컬럼 구분 헤더 추가
-    fprintf(fp1, "%8s %8s\n", "X", "Y");
-    printf("%8s %8s\n", "X", "Y");
-
-    fprintf(fp1, "%8d %8d\n", 0, 0);
-    printf("%8d %8d\n", 0, 0);
-
-    int i, x, y;
-    for (i = 0; i < MAX - 1; i++) {
-        x = rand() % MAX_RAND;
-        y = rand() % MAX_RAND;
-
-        fprintf(fp1, "%8d %8d\n", x, y);
-        printf("%8d %8d\n", x, y);
-    }
-
-    fclose(fp1);
-    printf("===================================================\n");
-    printf("\n[안내] 열려있는 메모장(01.txt)을 종료해야 다음 절차로 진행됩니다.\n");
-    system("notepad.exe 01.txt");
-}
--- 고예승/drone/run_step_01.c
+++ 고예승/drone/run_step_01.c
@@ -1,23 +1,29 @@
 #include "drone.h"
 
-void makeWayPoint()
-{
-	srand((unsigned int)time(NULL));
-	FILE* fp1 = fopen("01.txt", "w");
-	fprintf(fp1, "%8d %8d\n", 0, 0);
-	printf("%8d %8d\n", 0, 0);
+void run_step_01() {
+    FILE* fp1 = fopen("01.txt", "a+");
+    if (fp1 == NULL) return;
 
-	int i, x, y;
-	for (i = 0; i < MAX - 1; i++)
-	{
-		x = rand() % MAX_RAND;
-		y = rand() % MAX_RAND;
+    printf("\n웨이포인트 생성 결과:\n");
 
-		fprintf(fp1, "%8d %8d\n", x, y);
-		printf("%8d %8d\n", x, y);
-	}
+    // 파일과 화면 상단에 컬럼 구분 헤더 추가
+    fprintf(fp1, "%8s %8s\n", "X", "Y");
+    printf("%8s %8s\n", "X", "Y");
 
-	fclose(fp1);
-	printf("===================================================\n");
-	system("notepad.exe 01.txt");
+    fprintf(fp1, "%8d %8d\n", 0, 0);
+    printf("%8d %8d\n", 0, 0);
+
+    int i, x, y;
+    for (i = 0; i < MAX - 1; i++) {
+        x = rand() % MAX_RAND;
+        y = rand() % MAX_RAND;
+
+        fprintf(fp1, "%8d %8d\n", x, y);
+        printf("%8d %8d\n", x, y);
+    }
+
+    fclose(fp1);
+    printf("===================================================\n");
+    printf("\n[안내] 열려있는 메모장(01.txt)을 종료해야 다음 절차로 진행됩니다.\n");
+    system("notepad.exe 01.txt");
 }
